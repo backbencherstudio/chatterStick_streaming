fbe0c367e8a953e9c56d48d5819b260150ba70ab
Md. Rashedul Islam <santorasu09@gmail.com>
2025-10-28 08:56:13 +0600

    added a MVVM stracture

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../core/constant/route_names.dart';
+import '../../features/home/presentation/view/screen/home_screen.dart';
+import '../../features/splash/splash_screen.dart';
+
+class AppRoutes {
+  static const String initialRoute = RouteNames.splashScreen;
+
+  static final Map<String, WidgetBuilder> routes = {
+    RouteNames.splashScreen: (context) => const SplashScreen(),
+    RouteNames.home: (context) => const HomeScreen(),
+
+  };
+}
