84cd15adbaa3d040be8acfa276d63f2ec77c90c3
Arif <mdarifhosain214@gmail.com>
2025-11-03 10:20:31 +0600

    added api routes

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,13 +1,31 @@
 class ApiEndpoints {
-  static const String baseUrl =
-      "https://maui-intermediate-represented-option.trycloudflare.com";
-  static const String users = 'api/auth/register';
-   static const String login = 'api/auth/login';
-  static const String switchRole = 'api/auth/switch-role';
-  static const String verifyMail = 'api/auth/verify-email';
-  static const String resendOtp = 'api/auth/resend-verification-email';
-  static const String loadUser = 'api/auth/me';
-  static const String updateProfile = 'api/auth/update-profile';
-  static const String forgetPassword = 'api/auth/forgot-password';
-  static const String createAndagetJob = 'api/jobs';
+  //auth
+  static const String baseUrl = "http://192.168.4.199:8080/api";
+  static const String register = 'auth/register';
+  static const String login = 'auth/login';
+  static const String verifyMail = 'auth/verify-email';
+  static const String forgotPassword = 'auth/forgot-password';
+  static const String changePassword = 'auth/forgot-password';
+  static const String updateProfile = 'auth/update-profile';
+  static const String me = 'auth/me';
+  //profile
+  static const String lastRead = 'profile/last-reads';
+  static const String getMyIdea = 'profile/my-ideas';
+  static const String createIdea = 'profile/create-idea';
+  //comics
+  static const String singleEpisode = 'comics/episode';
+  static const String popularList = 'comics/popular-list';
+  static const String library = 'comics/';
+  static const String singleComics = 'comics/';
+  static const String popularComics = 'comics/popular';
+  //favorite
+  static const String favorite = 'favorite';
+  //download
+  static const String download = 'download';
+
+
+
+
+
+
 }
